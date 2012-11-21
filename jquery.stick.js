1d274baedae867bfb38b3d76d11b97f47443fd59
aarongustafson <aaron@easy-designs.net>
2012-11-21 14:18:24 -0500

    Updates to fix for multiple offsets

--- jquery.stick.js
+++ jquery.stick.js
@@ -1,83 +1,276 @@
-(function() {
-  var $, FIXED, STATIC, Stick;
+;(function( $, window, document ){
 
-  $ = jQuery;
+		// strings
+	var FIXED = 'fixed',
+		STATIC = 'static',
+		RELATIVE = 'relative',
+		IMPORTANT = ' !important;',
+		COLON = ':',
+		SIDES = 'top right bottom left'.split(' '),
+		POSITION = 'position',
+		MARGIN = 'margin-',
+		PX = 'px',
+		
+		// references
+		TRUE = true,
+		FALSE = false,
+		$win = $(window),
+		$body = $(document.body),
 
-  STATIC = 'static';
+		// default config
+		defaults = {
+			side: SIDES[0],
+			offset: 0,
+			zindex: 1000,
+			onStick: function(){},
+			onUnstick: function(){}
+		},
+		
+		// position: fixed support
+		supports_fixed = (function(){
+			
+			var $el = $('<div style="position:fixed;top:100px;">x</div>'),
+				el = $el.get(0),
+				o_height = $body.height(),
+				o_scrollTop = $body.scrollTop(),
+				// negativee, I know
+				supported = FALSE;
 
-  FIXED = 'fixed';
+			if ( 'getBoundingClientRect' in el )
+			{
+				// add the element to the document and scroll
+				$body
+					.append( $el )
+					.height(3000)
+					.scrollTop(500);
+				
+				// test to see if it moved
+				supported = ( el.getBoundingClientRect().top === 100 );
+				
+				// return to your normally-scheduled programming
+				$el.remove();
+				$body
+					.scrollTop( o_scrollTop )
+					.height( 'auto' );
+			}
 
-  $.fn.stick = function(args) {
-    var s;
-    return s = new Stick(this, args);
-  };
+			return supported;
+			
+		})(),
+		
+		// The object
+		Stick = (function(){
 
-  Stick = (function() {
+		  	function Stick( target, args )
+			{
+				this.target = target;
+				this.state = STATIC;
+				this.total_offset = 0;
+				this.width = target.width();
+				this.height = target.height();
+				
+				// get the original offset sides & margins
+				var i = SIDES.length,
+					offset,
+					offsets = {};
+				while ( i-- )
+				{
+					offset = target.css( SIDES[i] );
+					offsets[ SIDES[i] ] = offset == 'auto' ? 0 : offset;
+				}
+				this.original_offsets = offsets;
+				
+				// fixed takes the element out of the normal flow,
+				// we need to add it back in
+				if ( supports_fixed )
+				{
+					this.holder = $('<div class="sticky-holder"/>')
+										.hide()
+										.width( target.outerWidth() )
+										.height( target.outerHeight() );
+					target.after( this.holder );
+				}
+				
+				this.positioning = target.css( POSITION );
+				
+				if ( this.positioning == STATIC )
+				{
+					target.css( POSITION, RELATIVE );
+					this.positioning = RELATIVE;
+				}
+				
+				// config
+				if ( typeof args === "number" )
+				{
+					args = {
+						offset: args
+					};
+				}
+				$.extend( this, defaults, args );
+				
+				this.resize();
+				
+				(function(s){
+					return $win
+							.scroll(function(){
+								return s.scroll();
+							 })
+							.resize(function(){
+								return s.resize();
+							 });
+				})(this);
+			}
+			
+			Stick.prototype.position = function()
+			{
+				var position = supports_fixed ? FIXED : this.positioning,
+					offset = supports_fixed ? this.offset : this.total_offset;
+				
+				if ( supports_fixed &&
+					 this.state == FIXED )
+				{
+					this.holder.show();
+				}
+				else
+				{
+					this.holder.hide();
+				}
+				
+				return this.target.attr(
+					'style',
+					(
+						this.state == FIXED ?	POSITION + COLON + position + IMPORTANT +
+												this.side + COLON + offset + PX + IMPORTANT + 
+												'width' + COLON + this.width + PX + IMPORTANT + 
+												'height' + COLON + this.height + PX + IMPORTANT + 
+												'z-index: ' + this.zindex + IMPORTANT
+											:	''
+					)
+				);
+			};
 
-    function Stick(target, args) {
-      this.target = target;
-      this.offset = typeof args === "number" ? args : 0;
-      if (typeof args === "object") {
-        $.extend(this, args);
-      }
-      this.state = STATIC;
-      this.resize();
-      (function(s) {
-        return $(window).scroll(function() {
-          return s.scroll(this);
-        }).resize(function() {
-          return s.resize(this);
-        });
-      })(this);
-    }
+			Stick.prototype.resize = function()
+			{
+				var target_offset = this.target.offset(),
+					side = this.side,
+					height = $win.height();
+				
+				this.target_top = target_offset.top;
+				this.target_left = target_offset.left;
+				this.target_right = $win.width() - ( this.target_left + this.target.width() );
+				this.target_bottom = this.target_top + this.target.height();
 
-    Stick.prototype.position = function() {
-      return this.target.css({
-        "position": this.state,
-        "top": this.offset + "px",
-        "left": this.targetLeft + "px"
-      });
-    };
+				if ( ( side == 'top' &&
+					   height < this.target_bottom ) ||
+					 ( side == 'bottom' &&
+					   height > this.target_top ) )
+				{
+					this.disabled = TRUE;
+					if ( this.state === FIXED )
+					{
+						return this.position();
+					}
+				}
+				else
+				{
+					this.disabled = FALSE;
+					return this.scroll();
+				}
+			};
 
-    Stick.prototype.resize = function() {
-      var targetBottom;
-      this.targetOffset = parseInt(this.target.offset().top);
-      this.targetLeft = parseInt(this.target.offset().left);
-      targetBottom = this.targetOffset + this.target.height();
-      if ($(window).height() < targetBottom) {
-        this.disabled = true;
-        if (this.state === FIXED) {
-          return this.position();
-        }
-      } else {
-        this.disabled = false;
-        return this.scroll();
-      }
-    };
+			Stick.prototype.scroll = function()
+			{
+				
+				if ( this.disabled )
+				{
+					return;
+				}
+				
+				var TARGET = 'target_',
+					move = FALSE,
+					side = this.side,
+					target_offset = this[TARGET + side],
+					original_offset = this.original_offsets[side],
+					offset = this.offset,
+					body = $body.get(0),
+					scroll_top = $win.scrollTop() || 0,
+					scroll_left = $win.scrollLeft() || 0,
+					true_width = body.scrollWidth,
+					true_height = body.scrollHeight;
+				
+				switch ( side )
+				{
+					// top
+					case SIDES[0]:
 
-    Stick.prototype.scroll = function(self) {
-      if (this.disabled) {
-        return;
-      }
-      if ((this.targetOffset - $(self).scrollTop()) - this.offset <= 0) {
-        if (this.state === STATIC) {
-          if (typeof this.onStick === "function") {
-            this.onStick();
-          }
-        }
-        this.state = FIXED;
-      } else {
-        if (this.state === FIXED) {
-          if (typeof this.onUnstick === "function") {
-            this.onUnstick();
-          }
-        }
-        this.state = STATIC;
-      }
-      return this.position();
-    };
+						if ( target_offset - scroll_top - offset <= 0 )
+						{
+							move = TRUE;
+							this.total_offset = scroll_top - ( target_offset + offset + original_offset );
+						}
+						break;
 
-    return Stick;
+					// right
+					case SIDES[1]:
+					
+						if ( target_offset - ( true_width - $win.width() - scroll_left ) - offset <= 0 )
+						{
+							move = TRUE;
+							this.total_offset = true_width - ( target_offset + offset + original_offset );
+						}
+						break;
 
-  })();
-})();
+					// bottom
+					case SIDES[2]:
+						
+						if ( $win.height() + scroll_top > true_height - ( target_offset - offset ) )
+						{
+							move = TRUE;
+							this.total_offset = true_height - ( target_offset + offset + original_offset );
+						}
+						break;
+					
+					// left
+					case SIDES[3]:
+						
+						if ( target_offset - scroll_left - offset <= 0 )
+						{
+							move = TRUE;
+							this.total_offset = scroll_left - ( target_offset + offset + original_offset );
+						}
+						break;
+				}
+				
+				if ( move )
+				{
+					if ( this.state === STATIC )
+					{
+						this.onStick();
+					}
+					this.state = FIXED;
+				}
+				else
+				{
+					if ( this.state === FIXED )
+					{
+						this.onUnstick();
+					}
+					this.state = STATIC;
+				}
+				
+				return this.position();
+			};
+
+			return Stick;
+
+		})();
+	
+	$.fn.stick = function( args )
+	{
+		if ( this.length )
+		{
+			return new Stick( this, args );
+		}
+	};
+	
+})( jQuery, window, document );
